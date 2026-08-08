852647b042d9e12fcecdc09ef7fd4a19d6df0665
Kshama Kundargi <kshamakundaragi@gmail.com>
2026-08-08 21:24:42 +0530

    Time: 3 ms (17.48%), Space: 9.1 MB (35.87%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.c
@@ -0,0 +1,21 @@
+#include<string.h>
+bool isAnagram(char* s, char* t) {
+    if(strlen(s) != strlen(t))
+     return false;
+     int freq[26]= {0};
+     for(int i=0 ; s[i]!='\0'; i++)
+     {
+        freq[s[i]-'a']++;
+     }
+     for(int i=0; t[i]!='\0';i++)
+     {
+        freq[t[i]-'a']--;
+     }
+     for(int i=0; i<26 ;i++)
+     {
+        if(freq[i]!=0)
+        return false;
+     }
+     return true;
+    
+}
