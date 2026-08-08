e5a149dae9976f1bc14446510e8cda57cc41c7db
Kshama Kundargi <kshamakundaragi@gmail.com>
2026-08-08 21:19:23 +0530

    Time: 0 ms (100%), Space: 8.9 MB (64.38%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+bool isPalindrome(int x) {
+    if(x<0)
+    {
+        return false;
+    }
+    int og =x;
+   long long rev=0;
+    while(x>0)
+    {
+        int digit = x%10;
+        rev=rev*10+digit;
+        x=x/10;
+    }
+    if(og==rev)
+    {
+        return true;
+    }
+    else
+    return false;
+}
