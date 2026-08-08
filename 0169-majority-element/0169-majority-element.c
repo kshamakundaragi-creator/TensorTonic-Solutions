168f7211a3c41ba7f74a71ce0658c3ed35a1d8f8
Kshama Kundargi <kshamakundaragi@gmail.com>
2026-08-08 21:20:23 +0530

    Time: 0 ms (100%), Space: 10.9 MB (72.59%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.c
@@ -0,0 +1,25 @@
+int majorityElement(int* nums, int numsSize) {
+    int candidate;
+    int count =0;
+    
+    for(int i=0; i<numsSize; i++)
+    {
+        if(count==0)
+        {
+            candidate =nums[i];
+                count=1;
+
+        }
+      else{   
+        if(candidate == nums[i])
+       {
+         count++;
+       }
+       else{
+        count--;
+       }
+      }
+       
+    }
+    return candidate;
+}
